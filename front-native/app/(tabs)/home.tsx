4bca3798e8494f07eb6347bbb4f56b7210d64409
Maksym Boiko <maksym782@gmail.com>
2025-10-04 18:28:23 +0200

    tailwind work now

---
+++ front-native/app/(tabs)/home.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-xl">Hello, Tailwind!</Text>
+    </View>
+  );
+}
